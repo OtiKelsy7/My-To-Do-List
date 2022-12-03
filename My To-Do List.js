842c9c5e30f4ec174a5da06f6af160cb5b0fd904
OtiKelsy7 <107034055+OtiKelsy7@users.noreply.github.com>
2022-12-03 11:15:53 -0800

    new changes made

--- My To-Do List.js
+++ My To-Do List.js
@@ -1,4 +1,14 @@
+//initial reference
 const inputEl = document.querySelector("#input-field");
 const addButton = document.querySelector("#btn");
-console.log(inputEl)
-console.log(addButton)
+const display = document.querySelector("#display-div");
+const ulEl = document.querySelector("#ul-el");
+
+//adding event listeners to othe add button
+addButton.addEventListener("click", () => {
+    ulEl.innerHTML += `
+      <li>${inputEl.value}</li>
+      <i class="fa" aria-hidden="true">&#xf044;</i>
+    `
+    inputEl.value = "";
+})
