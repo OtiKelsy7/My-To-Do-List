2d69cfa58717aa276400fd2675c91b45b01aed31
Walter Tronics <waltertronics02@gmail.com>
2022-12-30 22:29:55 +0100

    Integration and restructure
    Integrate the edit functionality and restructure the create new task component

--- script.js
+++ script.js
@@ -8,64 +8,84 @@ const ulEl = document.getElementById("list-cont");
 //add button eventListener
 addBtn.addEventListener("click", () => {
 
+  const task = inputField.value;
      //This is a more better approach, no need for alert
-     if (inputField.value != '') {
-      //Creating a new list item
-      ulEl.innerHTML += `
-      <li class='list-item'>
-      <textarea disabled id='input-area'>${inputField.value}</textarea>
-       <div class='iconDiv'>
-        <button class="edit">
-         <i class="fas fa-pen-square"></i>
-        </button>
-        <button class="delete">
-         <i class="fas fa-trash"></i>
-        </button>
-       </div>
-      </li>
-      <hr>
-      `
-
-      //Calling the edit and delete function
-      edit();
-      del();
-
-      //clearing the input field
-      inputField.value = "";
-     }
-      
- 
-});
-
-
-
-
-//edit button function
-function edit() {
-  //Selecting all the edit buttons
-  let edit = document.querySelectorAll('.edit');
-
-  //Looping through all the edit buttons and assign each of them a click event
-  for (let i of edit) {
-    //Grab each element individually
-    i.addEventListener('click', ()=>{
-      //Get the input element for editing
-      let field = i.closest('li').querySelector('textarea');
-      //Check if it's current state is editing or not
-      if(field.hasAttribute('disabled')) {
-        //Edit the input
-        field.removeAttribute('disabled');
-        i.querySelector('i').classList.replace("fa-pen-square","fa-check-square");
-        field.focus();
-      }else{
-        //Close the editing
-        i.querySelector('i').classList.replace("fa-check-square","fa-pen-square");
-        field.setAttribute('disabled', '');
+     if (!task) {
+      return;
       }
-    });
-  }
+        
+        //clearing the input field
+        const list = document.createElement('li'),
+        textarea = document.createElement('textarea'),
+        btn_group = document.createElement('div'),
+        edit_btn = document.createElement('button'),
+        del_btn = document.createElement('button'),
+        edit_icon = document.createElement('i'),
+        del_icon = document.createElement('i');
+        
+        //Setting the textarea
+        textarea.setAttribute('disabled', '');
+        textarea.setAttribute('id','input-area');
+        textarea.value = task;
+
+        //Setting the edit and delete icon
+        edit_icon.setAttribute('class','');
+        edit_icon.classList.add('fas', 'fa-pen-square');
+        del_icon.setAttribute('class','');
+        del_icon.classList.add('fas', 'fa-trash');
+        
+        //Setting the edit button
+        edit_btn.setAttribute('class','edit');
+        edit_btn.appendChild(edit_icon);
+
+        //Setting the delete button
+        del_btn.setAttribute('class','delete');
+        del_btn.appendChild(del_icon);
+        
+        //Append the del and edit button to the group div and append to the list tag
+        btn_group.append(edit_btn,del_btn);
+        list.append(textarea,btn_group);
+        
+        //Add the list tag to the HTML
+        ulEl.appendChild(list);
+        //call the delete function on all tasks
+        del();
+        
+      
+      
+        edit_btn.addEventListener('click', (e)=>{
+          //Getting the input field for editing
+          let field = e.target.closest('li').querySelector('textarea');
+
+          //Check if it's current state is editing or not
+          if(field.hasAttribute('disabled')) {
+            //Edit the input
+            field.removeAttribute('disabled');
+            field.focus();
+            //console.log('open edit');
+
+            //Check if it's the button or the icon that was clicked and address both clicks
+            if (e.target.innerHTML !== ''){
+            e.target.querySelector('i').classList.replace("fa-pen-square","fa-check-square");
+            }else{e.target.classList.replace("fa-pen-square","fa-check-square")};
+          }else{
+            //Close the editing
+            field.setAttribute('disabled', '');
+            //console.log('close edit');
+
+            //Check for different element click
+            if (e.target.innerHTML !== ''){
+            e.target.querySelector('i').classList.replace("fa-check-square","fa-pen-square");
+            }else{e.target.classList.replace("fa-check-square","fa-pen-square")};
+          }
+        }, true);
+
+        inputField.value = "";
+        
+
+      });
+    
   
-}
 
 //Deleting tasks
 function del() {
@@ -74,8 +94,9 @@ function del() {
   
   //Looping through all the delete buttons and assign each of them a click event
   for (let i of del) {
-        //Grab each element individually
-        i.addEventListener('click', ()=>{
+    //Grab each element individually
+    i.addEventListener('click', ()=>{
+          //console.log('deleted');
           //Delete the task
           i.closest('li').remove();
         });
